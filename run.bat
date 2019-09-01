3ea4ebc1b00c487f160f49c9887ee9c06a9b653c
Felipe Barbosa <felipebbarbosa@gmail.com>
2019-09-01 17:54:33 -0300

    Parametrizado o disqus, googleads e o google analytics para não serem executados em ambiente de desenvolvimento

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
-bundle exec jekyll serve --drafts --watch --limit_posts 50
+@echo off
+
+rem bundle exec jekyll serve --drafts --watch --limit_posts 50
+
+echo Start vgscomcerveja!
+bundle exec jekyll serve --drafts --watch --config=_config.yml,_dev_config.yml
+
