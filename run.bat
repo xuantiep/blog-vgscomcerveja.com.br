944b513ae6e1383d9045a75bd28712cd18183520
Felipe Barbosa <felipebbarbosa@gmail.com>
2019-09-04 22:39:20 -0300

    Modificado o layout dos submenus; Ajustes no script de inicialização

--- run.bat
+++ run.bat
@@ -3,5 +3,12 @@
 rem bundle exec jekyll serve --drafts --watch --limit_posts 50
 
 echo Start vgscomcerveja!
-bundle exec jekyll serve --drafts --watch --config=_config.yml,_dev_config.yml
+set /p limitPosts=Deseja limitar a quantidade de posts? (y/n) 
+
+if /I %limitPosts%==y (
+    echo Limitando em 10 posts...
+    bundle exec jekyll serve --drafts --watch --config=_config.yml,_dev_config.yml --limit_posts 10
+) else (
+    bundle exec jekyll serve --drafts --watch --config=_config.yml,_dev_config.yml
+)
 
